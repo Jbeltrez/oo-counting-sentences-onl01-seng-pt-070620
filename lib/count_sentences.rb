32c3cfeef896e5ecb890224d7463132ab6212ee4
john beltrez <beltrezjohn@gmail.com>
2020-07-28 04:48:28 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,6 +3,9 @@ require 'pry'
 class String
 
   def sentence?
+    #returns true if the string you are calling it on ends in a period and false if it does not. #Use the self keyword to refer to the string on which the method is being called with. Use #end_with? method
+    self.end_with?(".")
+    return true 
     
     
   end
