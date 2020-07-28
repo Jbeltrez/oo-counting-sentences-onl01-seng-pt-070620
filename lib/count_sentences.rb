67a2d7c02df9d677579eca497eec85f6c556252e
john beltrez <beltrezjohn@gmail.com>
2020-07-28 05:25:00 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -36,6 +36,6 @@ class String
     #this method on a string should give us a count on the number of sentences in that string 
     #should use the self keyword to refer to the string on which it is called. 
     binding.pry
-    self.split([".","?", "!"])
+    self.split(".","?", "!")
   end
 end
